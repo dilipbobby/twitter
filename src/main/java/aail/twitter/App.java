0d61e10b3847e46644f4c2b5176c38021d9f2a38
dilipbobby <dilip14242@gmail.com>
2016-02-29 11:48:10 +0530

    tweetcommit

---
+++ src/main/java/aail/twitter/App.java
@@ -0,0 +1,14 @@
+package aail.twitter;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class App {
+
+	public static void main(String[] argv) {
+
+		System.out.println("Hello world");
+
+}
+}
